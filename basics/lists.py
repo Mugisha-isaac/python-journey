ca88fbba0ac756193034553e61b93efc8c734149
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-07-14 17:41:42 +0200

    ft: add lists

---
+++ basics/lists.py
@@ -0,0 +1,33 @@
+fruits_list = ["apple", "banana", "cherry"]
+# fruits_list[1:2] = ["blackcurrant", "watermelon"]
+# print(fruits_list)
+# fruits_list.remove("apple")
+# fruits_list.pop(2)
+# for fruit in fruits_list:
+#     print(fruit)
+# for i in range(len(fruits_list)):
+#     print(fruits_list[i])
+# list comprehension
+# new_list = [x for x in fruits_list if "a" in x]
+# print(new_list)
+# sort list
+# fruits = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# fruits.sort()
+# print(fruits)
+# sort descending
+# numbers = [23, 82, 1, 3, 45, 2, 4, 6, 7, 8, 9, 10]
+# numbers.sort(reverse=True)
+# print(numbers)
+# customizing sort function
+# def myFunc(n):
+#     return abs(n - 50)
+# numbers = [100, 50, 65, 82, 23]
+# numbers.sort(key=myFunc)
+# print(numbers)
+# copying a list
+# new_list = fruits_list.copy()
+# print(new_list)
+# Joining lists
+# list_one = ["a", "b", "c"]
+# list_two = [1, 2, 3]
+# print(list_one + list_two)
