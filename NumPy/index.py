1a8f94a82602814994c8deb13300dd18ad3fa4f5
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-07-16 10:49:17 +0200

    ft: numpy array basics

---
+++ NumPy/index.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# arr = np.array([1, 2, 3, 4, 5])
+# arr = np.array(42)
+
+# printing np version
+# print(np.__version__)
+# print(type(arr))
+
+# 2D array
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr[0][0])
