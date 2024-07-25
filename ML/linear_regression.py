c8856504d3a91218baa79e533f6828767ed7bcdb
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-07-25 16:53:43 +0200

    ft: add linear regression

---
+++ ML/linear_regression.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
+
+x = np.random.normal(5.0, 1.0, 1000)
+y = np.random.normal(10.0, 2.0, 1000)
+
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+
+def myFunc(x):
+    return slope * x + intercept
+
+
+myModel = list(map(myFunc, x))
+plt.scatter(x, y)
+plt.plot(x, myModel)
+plt.show()
